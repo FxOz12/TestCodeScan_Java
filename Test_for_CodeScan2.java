fc4bfaa5f7ccc7a2325b968bebf33436a75569c7
FxOz12 <123632267+FxOz12@users.noreply.github.com>
2023-01-26 16:37:26 +0100

    Update Test_for_CodeScan2.java

--- Test_for_CodeScan2.java
+++ Test_for_CodeScan2.java
@@ -1,4 +1,6 @@
-public class XSS extends HttpServlet {
+import java.io.IOException;
+import javax.servlet.*;
+class XSS extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
 		// BAD: a request parameter is written directly to the Servlet response stream
