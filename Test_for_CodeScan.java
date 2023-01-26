ed38f9bd07ccd34ba8b62591b59875885dd1bf85
FxOz12 <123632267+FxOz12@users.noreply.github.com>
2023-01-26 13:52:36 +0100

    Update Test_for_CodeScan.java

--- Test_for_CodeScan.java
+++ Test_for_CodeScan.java
@@ -19,8 +19,8 @@ class MyClass {
 
     public boolean equals(Object o) {
 		if (o == null) return false;
-		if (Customer.class != o.getClass()) return false;
-		Customer other = (Customer)o;
+		if (MyClass.class != o.getClass()) return false;
+		Myclass other = (MyClass)o;
 		if (!name.equals(o.name)) return false;
 		if (id != id) return false;  // Comparison of identical values
 		return true;
